bb6743cf95642901202766ed9c993b900c368b7d
John Whitton <john@johnwhitton.com>
2022-12-09 11:09:25 -0800

    Update secp256k1 and get benchmarks working

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -7,7 +7,7 @@ var eccjs = require('eccjs');
 var jodid = require('./deps/jodid');
 var ecdsa = require('ecdsa');
 var ECKey = require('eckey');
-var secp256k1 = require('secp256k1');
+var secp256k1 = require('secp256k1/elliptic');
 
 var benchmarks = [];
 var maxTime = 10;
@@ -20,7 +20,7 @@ function add(op, obj) {
 
       console.log('Benchmarking: ' + op);
       Object.keys(obj).forEach(function(key) {
-        suite.add(key + '#' + op, obj[key], { maxTime: maxTime })
+        suite.add(key + '#' + op, obj[key], { maxTime: maxTime });
       });
 
       suite
@@ -33,7 +33,7 @@ function add(op, obj) {
         })
         .run();
       console.log('========================');
-    }
+    },
   });
 }
 
@@ -49,10 +49,21 @@ function start() {
 
 var str = 'big benchmark against elliptic';
 
-var m1 = hash.sha256().update(str).digest();
-var c1 = elliptic.ec(elliptic.curves.secp256k1);
-var k1 = c1.genKeyPair();
-var s1 = c1.sign(m1, k1);
+// var m1 = hash.sha256().update(str).digest();
+const m1 = crypto.randomBytes(32);
+// var c1 = elliptic.ec(elliptic.curves.secp256k1);
+
+//var k1 = c1.genKeyPair();
+// generate privKey
+let privKey;
+do {
+  privKey = crypto.randomBytes(32);
+} while (!secp256k1.privateKeyVerify(privKey));
+
+// get the public key in a compressed format
+const pubKey = secp256k1.publicKeyCreate(privKey);
+
+var s1 = secp256k1.ecdsaSign(m1, privKey);
 var ok = '-----BEGIN EC PARAMETERS-----\n' +
          'BgUrgQQACg==\n' +
          '-----END EC PARAMETERS-----\n' +
@@ -61,93 +72,97 @@ var ok = '-----BEGIN EC PARAMETERS-----\n' +
          'oUQDQgAEM7DB6MG6T5nGSwougzGIXVypXb81EerftCyqTc7v/rjmLobz5ZJHGALh\n' +
          'ne+0Gdz5ZytFHQCTkUlU85Mz8SPjWg==\n' +
          '-----END EC PRIVATE KEY-----';
-assert(c1.verify(m1, s1, k1));
+assert(secp256k1.ecdsaVerify(s1.signature, m1, pubKey));
 
-var m2 = eccjs.sjcl.hash.sha256.hash('big benchmark against elliptic');
-var c2 = eccjs.sjcl.ecc.curves.k256;
-var k2 = eccjs.sjcl.ecc.ecdsa.generateKeys(c2, 0);
-var s2 = k2.sec.sign(m2, 0);
-assert(k2.pub.verify(m2, s2));
+// var m2 = eccjs.sjcl.hash.sha256.hash('big benchmark against elliptic');
+// var c2 = eccjs.sjcl.ecc.curves.k256;
+// var k2 = eccjs.sjcl.ecc.ecdsa.generateKeys(c2, 0);
+// var s2 = secp256k1.ecdsaSign(m1, k2.privKey);
+// // var s2 = k2.sec.ecdsaSign(m2, 0);
+// assert(k2.pub.verify(m2, s2));
 
-var m3 = crypto.createHash('sha256').update(str).digest();
-var k3 = new ECKey(crypto.randomBytes(32));
-var s3 = ecdsa.sign(m3, k3.privateKey);
-assert(ecdsa.verify(m3, s3, k3.publicKey));
+// var m3 = crypto.createHash('sha256').update(str).digest();
+// var k3 = new ECKey(crypto.randomBytes(32));
+// var s3 = ecdsa.ecdsaSign(m3, k3.privateKey);
+// assert(ecdsa.verify(m3, s3, k3.publicKey));
 
-var m4 = crypto.createHash('sha256').update(str).digest();
-var k4priv = crypto.randomBytes(32);
-var k4pub = secp256k1.createPublicKey(k4priv);
-var s4 = secp256k1.sign(k4priv, m4);
-assert(secp256k1.verify(k4pub, m4, s4) > 0);
+// var m4 = crypto.createHash('sha256').update(str).digest();
+// var k4priv = crypto.randomBytes(32);
+// var k4pub = secp256k1.publicKeyCreate(k4priv);
+// var s4 = secp256k1.ecdsaSign(k4priv, m4);
+// assert(secp256k1.verify(k4pub, m4, s4) > 0);
 
-add('sign', {
+add('ecdsaSign', {
   elliptic: function() {
-    c1.sign(m1, k1);
+    secp256k1.ecdsaSign(m1, privKey);
   },
-  sjcl: function() {
-    k2.sec.sign(m2, 0);
-  },
-  openssl: function() {
-    crypto.createSign('RSA-SHA256').update(str).sign(ok);
-  },
-  ecdsa: function() {
-    ecdsa.sign(m3, k3.privateKey);
-  },
-  secp256k1: function() {
-    secp256k1.sign(k4priv, m4);
-  }
+//   sjcl: function() {
+//     k2.sec.ecdsaSign(m2, 0);
+//   },
+//   openssl: function() {
+//     crypto.createecdsaSign('RSA-SHA256').update(str).ecdsaSign(ok);
+//   },
+//   ecdsa: function() {
+//     ecdsa.ecdsaSign(m3, k3.privateKey);
+//   },
+//   secp256k1: function() {
+//     secp256k1.ecdsaSign(k4priv, m4);
+//   },
 });
 
-var os1 = crypto.createSign('RSA-SHA256').update(str).sign(ok);
-var opk = '-----BEGIN PUBLIC KEY-----\n' +
-          'MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEM7DB6MG6T5nGSwougzGIXVypXb81Eerf\n' +
-          'tCyqTc7v/rjmLobz5ZJHGALhne+0Gdz5ZytFHQCTkUlU85Mz8SPjWg==\n' +
-          '-----END PUBLIC KEY-----';
-add('verify', {
-  elliptic: function() {
-    c1.verify(m1, s1, k1);
-  },
-  sjcl: function() {
-    k2.pub.verify(m2, s2);
-  },
-  openssl: function() {
-    crypto.createVerify('RSA-SHA256').update(str).verify(opk, os1);
-  },
-  ecdsa: function() {
-    ecdsa.verify(m3, s3, k3.publicKey);
-  },
-  secp256k1: function() {
-    secp256k1.verify(k4pub, m4, s4);
-  }
-});
+// var os1 = crypto.createecdsaSign('RSA-SHA256').update(str).ecdsaSign(ok);
+// var opk = '-----BEGIN PUBLIC KEY-----\n' +
+//           'MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEM7DB6MG6T5nGSwougzGIXVypXb81Eerf\n' +
+//           'tCyqTc7v/rjmLobz5ZJHGALhne+0Gdz5ZytFHQCTkUlU85Mz8SPjWg==\n' +
+//           '-----END PUBLIC KEY-----';
+// add('verify', {
+//   elliptic: function() {
+//     // c1.verify(m1, s1, k1);
+//     secp256k1.ecdsaVerify(s1.signature, m1, pubKey);
+//   },
+//   sjcl: function() {
+//     k2.pub.verify(m2, s2);
+//   },
+//   openssl: function() {
+//     crypto.createVerify('RSA-SHA256').update(str).verify(opk, os1);
+//   },
+//   ecdsa: function() {
+//     ecdsa.verify(m3, s3, k3.publicKey);
+//   },
+//   secp256k1: function() {
+//     secp256k1.verify(k4pub, m4, s4);
+//   },
+// });
 
 add('gen', {
   elliptic: function() {
-    c1.genKeyPair().getPublic();
+    pubKey;
+    // c1.genKeyPair().getPublic();
   },
-  sjcl: function() {
-    eccjs.sjcl.ecc.ecdsa.generateKeys(c2, 0);
-  }
+//   sjcl: function() {
+//     eccjs.sjcl.ecc.ecdsa.generateKeys(c2, 0);
+//   },
 });
 
 add('ecdh', {
   elliptic: function() {
-    c1.genKeyPair().derive(k1.getPublic());
-  }
-});
-
-var cu1 = elliptic.ec('curve25519');
-var ku1 = cu1.genKeyPair();
-var kp2 = jodid.eddsa.genKeySeed();
-
-add('curve25519', {
-  elliptic: function() {
-    var s = ku1.derive(cu1.genKeyPair().getPublic());
+    // c1.genKeyPair().derive(k1.getPublic());
+    pubKey;
   },
-  jodid: function() {
-    var s = jodid.dh.computeKey(kp2,
-                                jodid.dh.publicKey(jodid.eddsa.genKeySeed()));
-  }
 });
+
+// var cu1 = elliptic.ec('curve25519');
+// var ku1 = cu1.genKeyPair();
+// var kp2 = jodid.eddsa.genKeySeed();
+
+// add('curve25519', {
+//   elliptic: function() {
+//     var s = ku1.derive(cu1.genKeyPair().getPublic());
+//   },
+//   jodid: function() {
+//     var s = jodid.dh.computeKey(kp2,
+//       jodid.dh.publicKey(jodid.eddsa.genKeySeed()));
+//   },
+// });
 
 start();
